a190dcf45fa9948028152742afccd5838f85767f
EDWARD <edsztukowski@gmail.com>
2017-08-26 11:55:14 -0600

    getting latitude and longitude on page load now to auto display

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,28 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      test: 'test'
+      trailData: [],
+      lat: '',
+      long: ''
     }
+    this.getCoords = this.getCoords.bind(this);
+
+  }
+
+  getCoords = function() {
+    console.log('get coords fired')
+    navigator.geolocation.getCurrentPosition(function(location) {
+    this.setState(function() {
+      return {
+        lat: location.coords.latitude,
+        long: location.coords.longitude
+      }
+    })
+  }.bind(this));
+  }
+
+  componentDidMount() {
+    this.getCoords();
   }
 
 render() {
@@ -18,7 +38,7 @@ render() {
         jumboImage={require('./images/traily-jumbo.jpeg')}
         altTag='Hiking in the forest'
       />
-      <Search />
+      <Search  />
     </div>
   )
 }
