5a40bc283412a369f39f66af8708359fd656c098
EDWARD <edsztukowski@gmail.com>
2017-08-27 17:39:33 -0600

    added description

--- src/TrailList.js
+++ src/TrailList.js
@@ -9,9 +9,12 @@ function TrailList(props) {
           {trailArr.map(function(curr, index) {
             if (index < 15) {
               return (
-                <li key={curr.unique_id} className="trail-box col-md-4">
-                  {curr.name}
+                <div key={curr.unique_id}>
+                <li className="trail-box col-md-4">
+                  <h2>{curr.name}</h2>
+                  <p>{curr.activities[0].description}</p>
                 </li>
+                </div>
               )
             }
           })}
