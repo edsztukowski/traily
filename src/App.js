e382a319cb4639c65b1c94b1c6500d0a90484679
EDWARD <edsztukowski@gmail.com>
2017-08-31 19:57:16 -0600

    adding some routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-var Home = require('./Home')
+var Home = require('./Home');
+var TrailResults = require('./TrailResults')
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path = "/results" component={TrailResults} />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+
     );
   }
 }
